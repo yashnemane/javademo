23462e29312dabdbf0019b43b147dd996a35f319
yash.nemane-at-514304094524 <yash.nemane@infinitylearn.com>
2025-06-10 16:00:43 +0530

    sysout added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        System.out.println("Starting the Spring Boot application...");
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
+		System.out.println("Spring Boot application started successfully!");
+    }
 }
